3c743e9e2b0aa651b0043f91faa567584acfcf83
Divyesh Mali <medivyeshmali@gmail.com>
2024-08-09 22:52:18 +0530

    Signup & Login pages

---
+++ app/welcome.jsx
@@ -0,0 +1,96 @@
+import { StyleSheet, Text, View, Image, Pressable } from 'react-native'
+import React from 'react'
+import ScreenWrapper from './../components/ScreenWrapper';
+import { wp, hp } from '../helpers/common'
+import { StatusBar } from 'expo-status-bar';
+import { theme } from '../constants/theme';
+import { Button } from '../components/Button';
+import { useRouter } from 'expo-router';
+
+const welcome = () => {
+
+  const router = useRouter()
+
+  return (
+    <ScreenWrapper bg="white">
+      <StatusBar styyle="dark" />
+      <View style={styles.container}>
+        {/* Welcome Image  */}
+        <Image style={styles.welcomeImage} resizeMode='contain' source={require('../assets/images/welcome.png')} />
+
+        {/* Welcome Text */}
+        <Text style={styles.punchline}>Elevate upto the</Text>
+        <Text style={styles.title}>Welkin</Text>
+
+        {/* Footer */}
+        <View style={styles.footer}>
+          <Button 
+            title="Get Started"
+            buttonStyle={{ marginHorizontal: wp(3) }}
+            onPress={() => router.push('signUp')}
+          />
+          <View style={styles.bottomTextContainer}>
+            <Text style={styles.loginText}>
+              Already have an account?
+            </Text>
+            <Pressable onPress={() => router.push('login')}>
+              <Text style={[styles.loginText, {color: theme.colors.primaryDark, fontWeight: theme.fonts.semibold}]}>
+                Login
+              </Text>
+            </Pressable>
+          </View>
+        </View>
+      </View>
+    </ScreenWrapper>
+  )
+}
+
+export default welcome
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    paddingHorizontal: wp(4),
+  },
+
+  welcomeImage: {
+    height: hp(30),
+    width: wp(100),
+    alignSelf: 'center'
+  },
+
+  title: {
+    color: theme.colors.text,
+    fontSize: hp(4),
+    textAlign: 'center',
+    fontWeight: theme.fonts.extraBold
+  },
+
+  punchline: {
+    textAlign: 'center',
+    paddingHorizontal: wp(10),
+    fontSize: hp(1.7),
+    color: theme.colors.text
+  },
+
+  footer: {
+    gap: 30,
+    width: '100%',
+  },
+
+  bottomTextContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 5
+  },
+
+  loginText: {
+    textAlign: 'center',
+    color: theme.colors.text,
+    fontSize: hp(1.6),
+  }
+})
