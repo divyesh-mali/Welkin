a6f049e2bd56d8711ab8042e3f00c497e7a2754e
Divyesh Mali <medivyeshmali@gmail.com>
2024-08-13 20:02:01 +0530

    Corrected the style names

--- components/Input.jsx
+++ components/Input.jsx
@@ -6,7 +6,7 @@ import { wp, hp } from '../helpers/common'
 
 const Input = (props) => {
   return (
-    <View style={[styles.container, props.containerStyles && props.containerStyles]}>
+    <View style={[styles.container, props.containerStyle && props.containerStyle]}>
       {
         props.icon && props.icon
       }
